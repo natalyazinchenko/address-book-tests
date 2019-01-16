5a903011aa36109b95bdd1eb0f36f248dd7296fb
natalyazinchenko <natalyazinchenko@nixsolutions.com>
2019-01-16 15:03:32 +0200

    GroupModif and GroupDeletion added IF

--- src/main/java/tests/GroupDeletionTest.java
+++ src/main/java/tests/GroupDeletionTest.java
@@ -1,11 +1,19 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTest extends TestBase {
 
     @Test
     public void testDeletionGroup() throws Exception {
+        if (!app.getGroupHelper().isElementPresent(By.name("selected[]"))){
+            app.getGroupHelper().selectGroups();
+            app.getGroupHelper().clickNewGroupButton();
+            app.getGroupHelper().groupNameTyping();
+            app.getGroupHelper().submitChanges("submit");
+            app.getNavigationHelper().goBackToTheGroupsPage();
+        }
         app.getGroupHelper().selectGroups();
         app.getGroupHelper().selectAndDeleteGroup();
         app.getNavigationHelper().goBackToTheGroupsPage();
--- src/main/java/tests/GroupModificationTest.java
+++ src/main/java/tests/GroupModificationTest.java
@@ -1,11 +1,19 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class GroupModificationTest extends TestBase {
 
     @Test
     public void testGroupModification(){
+        if (!app.getGroupHelper().isElementPresent(By.name("selected[]"))){
+            app.getGroupHelper().selectGroups();
+            app.getGroupHelper().clickNewGroupButton();
+            app.getGroupHelper().groupNameTyping();
+            app.getGroupHelper().submitChanges("submit");
+            app.getNavigationHelper().goBackToTheGroupsPage();
+        }
         app.getGroupHelper().selectGroups();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().groupNameTyping();
