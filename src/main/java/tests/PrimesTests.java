c3a01b85d54cf5f45b9de4275d8590271057c795
natalyazinchenko <natalyazinchenko@nixsolutions.com>
2019-01-23 19:18:01 +0200

    testLongPrimes and testNonPrimes

--- src/main/java/tests/PrimesTests.java
+++ src/main/java/tests/PrimesTests.java
@@ -10,4 +10,15 @@ public class PrimesTests {
     public void testPrimes(){
         Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
     }
+
+    @Test
+    public void testNonPrimes(){
+        Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+    }
+
+    @Test
+    public void testLongPrimes(){
+        long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrimeLong(n));
+    }
 }
